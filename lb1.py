5d70e1e45d15a29d622525da493ce8d3e5a02636
Hanzila Rasool <muhammadhanzilarasool@gmail.com>
2024-07-01 19:59:26 -0700

    lab1 working on printing statement function and calling and conditional rendering , second commit

---
+++ lb1.py
@@ -0,0 +1,17 @@
+print("hey there, I am your program!")  # Corrected punctuation
+
+# text = input("Type something to test this out: ")  # Added colon
+# print(text)
+
+def myfunction():
+    """This is a docstring explaining the function's purpose (optional)."""
+    print("hello world")  # Added parentheses
+
+myfunction()  # Call the function
+
+x=-5
+# statements ke bd : ayega
+if(x>1):
+    print("x is positive")
+else:
+    print("x is less than")
